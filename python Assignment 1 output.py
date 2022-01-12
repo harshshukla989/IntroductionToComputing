47f7606a60097a602bcaaefca2a16a2aad408643
harshshukla989 <97080459+harshshukla989@users.noreply.github.com>
2022-01-13 01:11:29 +0530

    Add files via upload

---
+++ python Assignment 1 output.py
@@ -0,0 +1,56 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================================================================== RESTART: C:\Python310\python assignment.py ======================================================================
+Question 1 
+
+Enter the first number: 
+30
+Enter the second number: 
+50
+Enter the third number: 
+40
+The average of 3 numbers is: 40.0
+
+
+Question 2 
+
+Enter the Gross income in $: 
+25000
+Specify the Number of dependents:
+2
+The total income tax of the person is : $ 1800.0
+
+
+Question 3 
+
+Enter the SID: 
+21104102
+Enter the name of the student: 
+Harsh Shukla
+Enter the gender of the student:
+M
+Enter the Course Name:
+Electrical
+Enter the CGPA of the student:
+9.9
+[21104102, 'Harsh Shukla', 'M', 'Electrical', 9.9]
+
+
+Question 4 
+
+Enter the marks of the five students:
+96
+97
+93
+90
+92
+[90, 92, 93, 96, 97]
+
+
+Question 5 
+
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
+
+
