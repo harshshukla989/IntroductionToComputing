298a858bfa860a0ca50295609b387c434402311d
harshshukla989 <97080459+harshshukla989@users.noreply.github.com>
2022-02-14 20:30:01 +0530

    Add files via upload

---
+++ Python Assignment 3 Output.py
@@ -0,0 +1,64 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:/Python310/Assignment 3/Assignment 3/Python Assignment 3 code.py =
+Answer 1.
+Enter a string: Coding is really fun
+{'coding': 1, 'is': 1, 'really': 1, 'fun': 1}
+
+
+Answer 2.
+Day- 04
+Month- 05
+Year- 2003
+5 / 5 / 2003
+
+
+Answer 3.
+Enter a list of numbers: 9 5 17 12 5
+[(9, 81), (5, 25), (17, 289), (12, 144), (5, 25)]
+
+
+Answer 4.
+Enter Grade Point: 9
+Your Grade is 'A' and Excellent
+
+
+Answer 5.
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+Answer 6.
+Enter SID: 21104102
+Enter Name: Harsh Shukla
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21104108
+Enter Name: Samit Madhesia
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21104125
+Enter Name: Keshav Singh
+Do you want to enter another student entry(Y or N):N
+a. {21104102: 'Harsh Shukla', 21104108: 'Samit Madhesia', 21104125: 'Keshav Singh'}
+b. {21104102: 'Harsh Shukla', 21104125: 'Keshav Singh', 21104108: 'Samit Madhesia'}
+c. {21104102: 'Harsh Shukla', 21104108: 'Samit Madhesia', 21104125: 'Keshav Singh'}
+Search Name with SID: 21104102
+d. Harsh Shukla
+
+
+Answer 7.
+How long should be the sequence? 8
+[1, 1, 2, 3, 5, 8, 13, 21]
+The average of the series is  6.75
+
+
+Answer 8.
+a. {1, 3, 5, 6, 8}
+b. {3, 6, 8, 9, 13, 17}
+c. {1, 2, 4, 5}
+d. {6, 7, 8, 9, 10}
+e. {10, 7}
