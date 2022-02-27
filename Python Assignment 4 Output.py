6fa679a6140615165ba8f08b5343651dcc26a67b
harshshukla989 <97080459+harshshukla989@users.noreply.github.com>
2022-02-27 19:41:43 +0530

    Add files via upload

---
+++ Python Assignment 4 Output.py
@@ -0,0 +1,94 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:/Python Assignment 4/Python Assignment 4 Code.py =========
+
+[Question 1]
+
+Enter number of disk in tower of Hanoi:3
+
+The Disks are numbered starting from top of the tower.
+Steps to move all disks from Source Tower to Destination Tower is given below:
+
+Move disk 1 from Source Tower to Destination
+Move disk 2 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 3 from Source Tower to Destination
+Move disk 1 from Auxiliary to Source Tower
+Move disk 2 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+
+[Question.2](Recursive)
+The Pascle's Triangle using recursive approach is:
+
+Enter number of rows in Pascle's Triangle:3
+                                                 1                                                  
+                                                1  1                                                
+                                              1  2  1                                               
+
+[Question.2](Iterative)
+The Pascle's Triangle using iterative approach is:
+
+Enter number of rows:3
+                                                 1                                                  
+                                                1  1                                                
+                                              1  2  1                                               
+
+[Question.3]
+
+Enter an Integer:5
+Enter another Integer:4
+
+The quotient when 5 is divided by 4 is 1.
+The remainder when 5 is divided by 4 is 1.
+
+Que3.a
+Function is Not-callable
+
+Que3.b
+All result values are 'non-zero'
+
+Que3.c
+Appended (4,5,6) in the values (1,1)
+Values greater than 4 is 5,6
+
+Que3.d
+Above result in set form is shown below:
+{5, 6}
+
+Que3.e
+The above set has been converted to immutable.
+
+Que3.f
+Max value from set is 6
+Hash value of 6 is 6
+
+[Question.4]
+
+Hello, My name is Harsh and my roll no. is 21104102
+Destructor Called
+
+[Question.5]
+
+Employee Name is Mehak and Salary is 40000 
+Employee Name is Ashok and Salary is 50000 
+Employee Name is Viren and Salary is 60000 
+
+Que.5a
+Mehak salary Updated to 70000
+Employee Name is Mehak and Salary is 70000 
+
+Que.5b
+Employee Viren's data has been removed.
+
+[Question 6]
+
+          WELCOME TO THE FRIENDSHIP GAME
+
+Enter Player1 name:Harsh
+Enter Player2 name:Harshit
+
+Enter Word by Harsh:attack
+Enter Word by Harshit:attack
+
+Friendship of Harsh and Harshit is REAL
